35d63e3a558a6dbd9ab66af3b759bc5a55a873d5
Dmitriy Kozlov <kdmitriys@gmail.com>
2019-07-27 12:59:25 +0300

    Модуляция

--- js/setup.js
+++ js/setup.js
@@ -1,170 +1,176 @@
 'use strict';
 
-var ENTER_KEY = 13;
-var ESC_KEY = 27;
-var WISARDS_MAX = 4;
-var setupWindow = document.querySelector('.setup');
-var setupSimilar = document.querySelector('.setup-similar');
-var similarWizardTemplate = document
-  .querySelector('#similar-wizard-template')
-  .content.querySelector('.setup-similar-item');
+(function () {
+  var ENTER_KEY = 13;
+  var ESC_KEY = 27;
+  var WISARDS_MAX = 4;
+  var setupWindow = document.querySelector('.setup');
+  var setupSimilar = document.querySelector('.setup-similar');
+  var similarWizardTemplate = document
+    .querySelector('#similar-wizard-template')
+    .content.querySelector('.setup-similar-item');
 
-var setupOpenButton = document.querySelector('.setup-open');
-var setupOpenIcon = document.querySelector('.setup-open-icon');
-var setupCloseButton = document.querySelector('.setup-close');
-var setupWizardForm = document.querySelector('.setup-wizard-form');
+  var setupOpenButton = document.querySelector('.setup-open');
+  var setupOpenIcon = document.querySelector('.setup-open-icon');
+  var setupCloseButton = document.querySelector('.setup-close');
+  var setupWizardForm = document.querySelector('.setup-wizard-form');
 
-// Изображение персонажа в окне настроек
-var wizardSetup = {
-  eyes: document.querySelector('.setup-wizard .wizard-eyes'),
-  coat: document.querySelector('.setup-wizard .wizard-coat'),
-  fireball: document.querySelector('.setup-fireball-wrap')
-};
+  // Изображение персонажа в окне настроек
+  var wizardSetup = {
+    eyes: document.querySelector('.setup-wizard .wizard-eyes'),
+    coat: document.querySelector('.setup-wizard .wizard-coat'),
+    fireball: document.querySelector('.setup-fireball-wrap')
+  };
 
-// Инициализируем набор возможных свойств персонажей
-var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+  // Инициализируем набор возможных свойств персонажей
+  var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 
-var surnames = [
-  'да Марья',
-  'Верон',
-  'Мирабелла',
-  'Вальц',
-  'Онопко',
-  'Топольницкая',
-  'Нионго',
-  'Ирвинг'
-];
+  var surnames = [
+    'да Марья',
+    'Верон',
+    'Мирабелла',
+    'Вальц',
+    'Онопко',
+    'Топольницкая',
+    'Нионго',
+    'Ирвинг'
+  ];
 
-var coatColors = [
-  'rgb(101, 137, 164)',
-  'rgb(241, 43, 107)',
-  'rgb(146, 100, 161)',
-  'rgb(56, 159, 117)',
-  'rgb(215, 210, 55)',
-  'rgb(0, 0, 0)'
-];
+  var coatColors = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ];
 
-var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+  var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var fireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+  var fireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
-// Возвращает случайный элемент массива
-function getRandomArrayElement(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
-
-// Генерирует массив волшебников со случайними свойствами
-function generateRandomWizards() {
-  var randomWizardsArray = [];
-  for (var i = 0; i < WISARDS_MAX; i++) {
-    randomWizardsArray[i] = {
-      name: getRandomArrayElement(names) + ' ' + getRandomArrayElement(surnames),
-      coatColor: getRandomArrayElement(coatColors),
-      eyesColor: getRandomArrayElement(eyesColors)
-    };
+  // Возвращает случайный элемент массива
+  function getRandomArrayElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
   }
-  return randomWizardsArray;
-}
 
-// Создает DOM-элемент из объекта
-function getWizardItem(wizard) {
-  var wizardElement = similarWizardTemplate.cloneNode(true);
-  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
-  return wizardElement;
-}
-
-// Добавляет массив волшебников на страницу
-function addWizards(wizardsArray) {
-  var similarWizardsList = document.querySelector('.setup-similar-list');
-
-  for (var i = 0; i < wizardsArray.length; i++) {
-    similarWizardsList.appendChild(getWizardItem(wizardsArray[i]));
+  // Генерирует массив волшебников со случайними свойствами
+  function generateRandomWizards() {
+    var randomWizardsArray = [];
+    for (var i = 0; i < WISARDS_MAX; i++) {
+      randomWizardsArray[i] = {
+        name: getRandomArrayElement(names) + ' ' + getRandomArrayElement(surnames),
+        coatColor: getRandomArrayElement(coatColors),
+        eyesColor: getRandomArrayElement(eyesColors)
+      };
+    }
+    return randomWizardsArray;
   }
-}
 
-// Открывает окно настроек персонажа
-function openSetup() {
-  setupWindow.classList.remove('hidden');
-  setupSimilar.classList.remove('hidden');
-  setupOpenButton.removeEventListener('click', onSetupOpenButtonClick);
-  setupOpenIcon.removeEventListener('keydown', onSetupOpenIconEnterPress);
-  setupCloseButton.addEventListener('click', onSetupCloseButtonClick);
-  setupCloseButton.addEventListener('keydown', onExitSetupButtonEnterPress);
-  window.addEventListener('keydown', onSetupEscPress);
-  setupOpenIcon.setAttribute('tabindex', '-1');
-  setupCloseButton.setAttribute('tabindex', '0');
-}
-
-// Закрывает окно настроек персонажа
-function closeSetup() {
-  setupWindow.classList.add('hidden');
-  setupSimilar.classList.add('hidden');
-  setupCloseButton.removeEventListener('click', onSetupCloseButtonClick);
-  window.removeEventListener('keydown', onSetupEscPress);
-  setupOpenButton.addEventListener('click', onSetupOpenButtonClick);
-  setupOpenIcon.addEventListener('keydown', onSetupOpenIconEnterPress);
-  setupOpenIcon.setAttribute('tabindex', '0');
-  setupCloseButton.setAttribute('tabindex', '-1');
-  setupWindow.style.left = '';
-  setupWindow.style.top = '';
-}
-
-// Обработчики событий
-
-function onSetupOpenButtonClick(evt) {
-  evt.preventDefault();
-  openSetup();
-}
-
-function onSetupCloseButtonClick(evt) {
-  evt.preventDefault();
-  closeSetup();
-}
-
-function onSetupEscPress(evt) {
-  if (evt.keyCode === ESC_KEY &&
-    !(setupWizardForm.querySelector('.setup-user-name') === document.activeElement)) {
-    evt.preventDefault();
-    closeSetup();
+  // Создает DOM-элемент из объекта
+  function getWizardItem(wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    return wizardElement;
   }
-}
 
-function onExitSetupButtonEnterPress(evt) {
-  if (evt.keyCode === ENTER_KEY) {
-    evt.preventDefault();
-    closeSetup();
+  // Добавляет массив волшебников на страницу
+  function addWizards(wizardsArray) {
+    var similarWizardsList = document.querySelector('.setup-similar-list');
+
+    for (var i = 0; i < wizardsArray.length; i++) {
+      similarWizardsList.appendChild(getWizardItem(wizardsArray[i]));
+    }
   }
-}
 
-function onSetupOpenIconEnterPress(evt) {
-  if (evt.keyCode === ENTER_KEY) {
+  // Открывает окно настроек персонажа
+  function openSetup() {
+    setupWindow.classList.remove('hidden');
+    setupSimilar.classList.remove('hidden');
+    setupOpenButton.removeEventListener('click', onSetupOpenButtonClick);
+    setupOpenIcon.removeEventListener('keydown', onSetupOpenIconEnterPress);
+    setupCloseButton.addEventListener('click', onSetupCloseButtonClick);
+    setupCloseButton.addEventListener('keydown', onExitSetupButtonEnterPress);
+    window.addEventListener('keydown', onSetupEscPress);
+    setupOpenIcon.setAttribute('tabindex', '-1');
+    setupCloseButton.setAttribute('tabindex', '0');
+  }
+
+  // Закрывает окно настроек персонажа
+  function closeSetup() {
+    setupWindow.classList.add('hidden');
+    setupSimilar.classList.add('hidden');
+    setupCloseButton.removeEventListener('click', onSetupCloseButtonClick);
+    window.removeEventListener('keydown', onSetupEscPress);
+    setupOpenButton.addEventListener('click', onSetupOpenButtonClick);
+    setupOpenIcon.addEventListener('keydown', onSetupOpenIconEnterPress);
+    setupOpenIcon.setAttribute('tabindex', '0');
+    setupCloseButton.setAttribute('tabindex', '-1');
+    setupWindow.style.left = '';
+    setupWindow.style.top = '';
+  }
+
+  // Обработчики событий
+
+  function onSetupOpenButtonClick(evt) {
     evt.preventDefault();
     openSetup();
   }
-}
 
-function onEyesClick(evt) {
-  evt.target.style.fill = getRandomArrayElement(eyesColors);
-}
+  function onSetupCloseButtonClick(evt) {
+    evt.preventDefault();
+    closeSetup();
+  }
 
-function onCoatClick(evt) {
-  evt.target.style.fill = getRandomArrayElement(coatColors);
-}
+  function onSetupEscPress(evt) {
+    if (
+      evt.keyCode === ESC_KEY &&
+      !(setupWizardForm.querySelector('.setup-user-name') === document.activeElement)
+    ) {
+      evt.preventDefault();
+      closeSetup();
+    }
+  }
 
-function onFireballClick(evt) {
-  evt.target.style.backgroundColor = getRandomArrayElement(fireballColors);
-}
+  function onExitSetupButtonEnterPress(evt) {
+    if (evt.keyCode === ENTER_KEY) {
+      evt.preventDefault();
+      closeSetup();
+    }
+  }
 
-// Выводим случайных волшебников на страницу
-addWizards(generateRandomWizards());
+  function onSetupOpenIconEnterPress(evt) {
+    if (evt.keyCode === ENTER_KEY) {
+      evt.preventDefault();
+      openSetup();
+    }
+  }
 
-// Добавляем обработчики событий
-setupOpenButton.addEventListener('click', onSetupOpenButtonClick);
-setupOpenIcon.addEventListener('keydown', onSetupOpenIconEnterPress);
-wizardSetup.eyes.addEventListener('click', onEyesClick);
-wizardSetup.coat.addEventListener('click', onCoatClick);
-wizardSetup.fireball.addEventListener('click', onFireballClick);
+  function onEyesClick(evt) {
+    evt.target.style.fill = getRandomArrayElement(eyesColors);
+  }
+
+  function onCoatClick(evt) {
+    evt.target.style.fill = getRandomArrayElement(coatColors);
+  }
+
+  function onFireballClick(evt) {
+    evt.target.style.backgroundColor = getRandomArrayElement(fireballColors);
+  }
+
+  // Выводим случайных волшебников на страницу
+  addWizards(generateRandomWizards());
+
+  // Добавляем обработчики событий
+  setupOpenButton.addEventListener('click', onSetupOpenButtonClick);
+  setupOpenIcon.addEventListener('keydown', onSetupOpenIconEnterPress);
+  wizardSetup.eyes.addEventListener('click', onEyesClick);
+  wizardSetup.coat.addEventListener('click', onCoatClick);
+  wizardSetup.fireball.addEventListener('click', onFireballClick);
+
+})();
+
 
 // EOF
--- js/stat.js
+++ js/stat.js
@@ -1,109 +1,129 @@
 'use strict';
 
-var renderStatistics = function (ctx, names, times) {
-  // Настройки окна статистики
-  var sets = {
-    fontFamily: 'PT Mono',
-    defaultFontSize: 16,
-    defaultColor: 'black',
-    defaultBgColor: 'white',
-    modalWindow: {
-      coordX: 100,
-      coordY: 10,
-      width: 420,
-      height: 270,
-      title: 'Ура вы победили!\nСписок результатов:'
-    },
-    shadow: {
-      shift: 10,
-      color: 'rgba(0, 0, 0, 0.7)'
-    },
-    hist: {
-      height: 150,
-      columnWidth: 40,
-      columnsGap: 50,
-      currentColor: 'rgba(255, 0, 0, 1)',
-      getOtherColor: function () {
-        return 'hsl(240,' + Math.random() * 100 + '%, 50%)';
+(function () {
+  var renderStatistics = function (ctx, names, times) {
+    // Настройки окна статистики
+    var sets = {
+      fontFamily: 'PT Mono',
+      defaultFontSize: 16,
+      defaultColor: 'black',
+      defaultBgColor: 'white',
+      modalWindow: {
+        coordX: 100,
+        coordY: 10,
+        width: 420,
+        height: 270,
+        title: 'Ура вы победили!\nСписок результатов:'
+      },
+      shadow: {
+        shift: 10,
+        color: 'rgba(0, 0, 0, 0.7)'
+      },
+      hist: {
+        height: 150,
+        columnWidth: 40,
+        columnsGap: 50,
+        currentColor: 'rgba(255, 0, 0, 1)',
+        getOtherColor: function () {
+          return 'hsl(240,' + Math.random() * 100 + '%, 50%)';
+        }
       }
-    }
-  };
-
-  // Определяет максимальный элемент-число массива
-  var maxNumberArrayElement = function (numArray) {
-    var maxElement = numArray[0];
-    for (var i = 0; i < numArray.length; i++) {
-      if (numArray[i] > maxElement) {
-        maxElement = numArray[i];
-      }
-    }
-    return maxElement;
-  };
-
-  // Вывод модального окна с тенью
-  var drawModalWindow = function (context, x, y, width, height, bgColor) {
-    var currentContextFillStyle = ctx.fillStyle;
-
-    context.fillStyle = sets.shadow.color;
-    context.fillRect(x + sets.shadow.shift, y + sets.shadow.shift, width, height);
-    context.fillStyle = bgColor ? bgColor : sets.defaultBgColor;
-    context.strokeRect(x, y, width, height);
-    context.fillRect(x, y, width, height);
-
-    context.fillStyle = currentContextFillStyle;
-  };
-
-  // Вывод многострочного текста в указанный контекст
-  // Переносы строк по символу конца строки из текста
-  var printText = function (context, text, x, y, fontSize, color) {
-    var currentContextFillStyle = context.fillStyle; // сохраняем текущий стиль чтобы потом вернуть
-    context.font = fontSize + 'px ' + sets.fontFamily;
-    context.fillStyle = color ? color : sets.defaultColor;
-    var strings = (text + '').split('\n');
-
-    // Выводим отдельные строки
-    for (var i = 0; i < strings.length; i++) {
-      context.fillText(strings[i], x, y + fontSize * i);
-    }
-
-    context.fillStyle = currentContextFillStyle;
-  };
-
-  // Рисует гистограмму в заданном месте
-  var drawHistogram = function (context, x, y, namesArray, timesArray) {
-    // Находим коэффициент пропорциональности для высоты гистограммы с учетом надписей у каждой колонки
-    var ratio = maxNumberArrayElement(timesArray) / (sets.hist.height - sets.defaultFontSize * 2);
-
-    // Выводит колонку с надписями, для имени "Вы" назначает специальный цвет
-    var drawBar = function (count, name, time) {
-      var barHeight = Math.floor(time / ratio); // высота колонки в пропорции
-      var barX = x + count * (sets.hist.columnWidth + sets.hist.columnsGap); // начальная координата X
-      var barY = y + sets.hist.height - sets.defaultFontSize * 2 - barHeight; // yачальная координата Y
-
-      printText(context, Math.floor(time), barX, barY, sets.defaultFontSize);
-      context.fillStyle = name === 'Вы' ? sets.hist.currentColor : sets.hist.getOtherColor();
-      context.fillRect(barX, barY + sets.defaultFontSize, sets.hist.columnWidth, barHeight);
-      printText(
-          context,
-          name,
-          barX,
-          barY + sets.defaultFontSize * 2 + barHeight,
-          sets.defaultFontSize
-      );
     };
 
-    // Выводим все колонки гистограммы в цикле
-    for (var i = 0; i < namesArray.length; i++) {
-      drawBar(i, namesArray[i], timesArray[i]);
-    }
+    // Определяет максимальный элемент-число массива
+    var maxNumberArrayElement = function (numArray) {
+      var maxElement = numArray[0];
+      for (var i = 0; i < numArray.length; i++) {
+        if (numArray[i] > maxElement) {
+          maxElement = numArray[i];
+        }
+      }
+      return maxElement;
+    };
+
+    // Вывод модального окна с тенью
+    var drawModalWindow = function (context, x, y, width, height, bgColor) {
+      var currentContextFillStyle = ctx.fillStyle;
+
+      context.fillStyle = sets.shadow.color;
+      context.fillRect(x + sets.shadow.shift, y + sets.shadow.shift, width, height);
+      context.fillStyle = bgColor ? bgColor : sets.defaultBgColor;
+      context.strokeRect(x, y, width, height);
+      context.fillRect(x, y, width, height);
+
+      context.fillStyle = currentContextFillStyle;
+    };
+
+    // Вывод многострочного текста в указанный контекст
+    // Переносы строк по символу конца строки из текста
+    var printText = function (context, text, x, y, fontSize, color) {
+      var currentContextFillStyle = context.fillStyle; // сохраняем текущий стиль чтобы потом вернуть
+      context.font = fontSize + 'px ' + sets.fontFamily;
+      context.fillStyle = color ? color : sets.defaultColor;
+      var strings = (text + '').split('\n');
+
+      // Выводим отдельные строки
+      for (var i = 0; i < strings.length; i++) {
+        context.fillText(strings[i], x, y + fontSize * i);
+      }
+
+      context.fillStyle = currentContextFillStyle;
+    };
+
+    // Рисует гистограмму в заданном месте
+    var drawHistogram = function (context, x, y, namesArray, timesArray) {
+      // Находим коэффициент пропорциональности для высоты гистограммы с учетом надписей у каждой колонки
+      var ratio = maxNumberArrayElement(timesArray) / (sets.hist.height - sets.defaultFontSize * 2);
+
+      // Выводит колонку с надписями, для имени "Вы" назначает специальный цвет
+      var drawBar = function (count, name, time) {
+        var barHeight = Math.floor(time / ratio); // высота колонки в пропорции
+        var barX = x + count * (sets.hist.columnWidth + sets.hist.columnsGap); // начальная координата X
+        var barY = y + sets.hist.height - sets.defaultFontSize * 2 - barHeight; // yачальная координата Y
+
+        printText(context, Math.floor(time), barX, barY, sets.defaultFontSize);
+        context.fillStyle = name === 'Вы' ? sets.hist.currentColor : sets.hist.getOtherColor();
+        context.fillRect(barX, barY + sets.defaultFontSize, sets.hist.columnWidth, barHeight);
+        printText(
+            context,
+            name,
+            barX,
+            barY + sets.defaultFontSize * 2 + barHeight,
+            sets.defaultFontSize
+        );
+      };
+
+      // Выводим все колонки гистограммы в цикле
+      for (var i = 0; i < namesArray.length; i++) {
+        drawBar(i, namesArray[i], timesArray[i]);
+      }
+    };
+
+    // Поочередно выводим модальное окно, печатаем заголовок и рисуем гистограмму
+    drawModalWindow(
+        ctx,
+        sets.modalWindow.coordX,
+        sets.modalWindow.coordY,
+        sets.modalWindow.width,
+        sets.modalWindow.height
+    );
+    printText(
+        ctx,
+        sets.modalWindow.title,
+        sets.modalWindow.coordX + 20,
+        sets.modalWindow.coordY + 30,
+        sets.defaultFontSize
+    );
+    drawHistogram(
+        ctx,
+        sets.modalWindow.coordX + 40,
+        sets.modalWindow.coordY + sets.defaultFontSize + 70,
+        names,
+        times
+    );
   };
 
-  // Поочередно выводим модальное окно, печатаем заголовок и рисуем гистограмму
-  drawModalWindow(ctx, sets.modalWindow.coordX, sets.modalWindow.coordY, sets.modalWindow.width, sets.modalWindow.height);
-  printText(ctx, sets.modalWindow.title, sets.modalWindow.coordX + 20, sets.modalWindow.coordY + 30, sets.defaultFontSize);
-  drawHistogram(ctx, sets.modalWindow.coordX + 40, sets.modalWindow.coordY + sets.defaultFontSize + 70, names, times);
-};
-
-window.renderStatistics = renderStatistics;
+  window.renderStatistics = renderStatistics;
+})();
 
 // /EOF
